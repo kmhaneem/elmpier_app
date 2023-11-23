b73212cb866b0b7adb042ff571d2bbff4ca118a3
kmhaneem <kmhaneem00@gmail.com>
2023-11-23 15:40:53 +0530

    Remove api.dart and update .gitignore

--- frontend/lib/infrastructure/core/api.dart
+++
@@ -1,5 +0,0 @@
-const api2w = "http://192.168.8.1:3000";
-const api2 = "http://172.20.10.3:3000";
-const api2xc = "http://192.168.0.131:3000";
-const api222 = "http://192.168.8.190:3000";
-const api2x = "http://192.168.8.131:3000";
